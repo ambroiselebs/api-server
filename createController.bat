1ec8306038283e303d63a81bddeaa7fc0da6241f
ambroiselebs <amberenger06@gmail.com>
2022-11-11 09:35:12 +0100

    upgraded createController script

--- createController.bat
+++ createController.bat
@@ -4,8 +4,18 @@ if ["%1"] == [""] (
     goto end
 ) else (
     (
+    echo(import fs from ^'fs^'
+    echo(
+    echo(let rowData ^= fs^.readFileSync^(^'^.^/src^/config^.json^'^)
+    echo(let config ^= JSON^.parse^(rowData^)
+    echo(
     echo(export const %1 ^= ^(req ^,res^) ^=^> ^{
-    echo(    res.send^("%1"^)
+    echo(    if ^(req^.params^.token ^=^= config^.server^.token^)
+    echo(    ^{
+    echo(        res^.send^(^{data:^"%1^"^}^)
+    echo(    ^} else ^{
+    echo(        res^.send^(^{data:^"invalidAPItoken^"^}^)
+    echo(    ^}
     echo(^}
     )>src/controllers/%1.js
 )
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import bodyParser from 'body-parser';
-import { Performance } from 'perf_hooks';
 import express from 'express'
 
 import { routes } from './src/routes.js';
@@ -21,15 +20,8 @@ app.use(function(req, res, next) {
 //----------------------- END -----------------------//
 
 
-//----------------------- DataBase -----------------------//
-
-//----------------------- END -----------------------//
-
-
 //----------------------- Routes -----------------------//
-
 routes(app, fs) //Edit them in | ./src/routes.js |
-
 //----------------------- END -----------------------//
 
 
